47f972d40046ae650870480e99c8c2eff3fa85bc
Joonas Sarapalo <joonas.sarapalo@gmail.com>
2017-09-05 18:21:10 +0300

    Added an input field and the ability to process backend responces.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './grappa.jpg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePost = this.handlePost.bind(this);
+    this.state = {
+      username: "ennen kutsua"
+    }
+  }
+
+  handlePost(e) {
+    e.preventDefault()
+    let value = this.refs.input.value;
+    console.log("value", value)
+    axios.get('/helloUser?username=' + value)
+      .then(resp => {
+      this.setState({
+        username: resp.data.text
+      })
+    }).catch((error) => console.error(error));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Enter your name below</h2>
         </div>
+        <h2>{this.state.username}</h2>
         <p className="App-intro">
-        <form action="localhost:3100/helloUser">
-          Nimesi: <br />
-          <input type="text" name="username" /> <br />
-          <input type="submit" value="Lähetä" />
+        <form onSubmit={this.handlePost}>
+          <input ref="input" placeholder="enter your name here" type="text" name="username"/> <br />
+          <br />
+          <input type="submit" value="send" />
         </form>
         </p>
 
