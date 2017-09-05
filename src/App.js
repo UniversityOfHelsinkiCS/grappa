8e8e0b61ae74899c7519ef0dae91377002aaae58
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-09-05 10:58:56 +0300

    added form which asks name

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <form action="/localhost:3000">
+          Nimesi: <br />
+          <input type="text" name="nimi" /> <br />
+          <input type="submit" value="Lähetä" />
+
+
+        </form>
         </p>
+
       </div>
     );
   }
