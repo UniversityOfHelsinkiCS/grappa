4875a23caac6271ad0159426b679e35dde7478ea
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-09-13 11:39:56 +0300

    removed unused chai

--- test/test.js
+++ test/test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import test from 'ava';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
-import chai from 'chai';
-var expect = chai.expect;
 
 test('my passing test', t => {
 	t.pass();
