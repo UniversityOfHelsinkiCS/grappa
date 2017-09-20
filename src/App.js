7c5e28be6f44516e89cb24f3c5723cf0e3d9b0d8
Poletaev Dmitry <poletaev@dat-14.cs.helsinki.fi>
2017-09-20 14:43:34 +0300

    duplicated code removed

--- src/App.js
+++ src/App.js
@@ -17,19 +17,6 @@ var getAxios = () => {
 
 const service = getAxios();
 
-var getAxios = () => {
-  if (process.env.API_URL) {
-    console.log("not undefined");
-    return axios.create({
-      baseURL: process.env.API_URL
-    });
-  } else {
-    return axios;
-  }
-}
-
-const service = getAxios();
-
 class App extends Component {
   constructor(props) {
     super(props);
