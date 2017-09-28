c8c786e60bd5f98e3d3b31709c9407fc89a42eff
Poletaev Dmitry <poletaev@da3-cs-dk110-09.pc.helsinki.fi>
2017-09-28 13:52:20 +0300

    created test (still unworkable) for test saveNewContract Dao function

--- test/ContractDao.spec.js
+++ test/ContractDao.spec.js
@@ -1,9 +1,40 @@
 import test from 'ava';
+import sinon from 'sinon';
 
 const contractDao = require('../src/dao/ContractDao');
 const mockContracts = require('../src/mockdata/MockContracts');
 
 test.skip('ContractDao returns a contract by id correctly', t => {
+    let id = '1';
+    let contract = contractDao.getContractById(id);
+    let mockContract;
+    for(let i = 0; i < mockContracts.length; i++) {
+        if (mockContracts[i].id.toString() === id) {
+            mockContract = mockContracts[i];
+        }
+    }
+    t.deepEqual(contract, mockContract);
+});
+
+test.skip('saveNewContract calls knex correctly & returns correct json', t => {
+    const testData = {};
+    const knexAPI = {
+        returning: function () {},
+        insert: function () {},
+        then: function () {},
+    };
+    const knexMock = {};
+
+
+    knex('contract')
+    .returning('contractId')
+    .insert(data)
+    .then((contractId) => {
+        return {text: 'New contract saved to backend', contractId: contractId[0]};
+        
+      });
+
+
     let id = '1';
     let contract = contractDao.getContractById(id);
     let mockContract;
