e36fbfa0c73ee79eca896cea82875bdf524280e0
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-09-29 11:42:29 +0300

    removed ContractReducer.js

--- src/components/ContractReducer.js
+++
@@ -1,9 +0,0 @@
-
-export default function(state, action) {
-    switch(action.type) {
-        case "CONTRACT_SAVE_SUCCESS" :
-            console.log("Talletettu onnistuneesti");
-        case "CONTRACT_SAVE_FAILURE" :
-            console.log("Talletus epäonnistui.");
-  }
-}
