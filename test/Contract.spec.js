a86e3780eb0aae5bc687d7c16793f722e3e15a59
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-10-02 11:57:36 +0300

    fixed broken path name

--- test/Contract.spec.js
+++ test/Contract.spec.js
@@ -5,7 +5,7 @@ import { shallow } from 'enzyme';
 //import ava from 'ava';
 import sinon from 'sinon';
 require('ignore-styles')
-import Contract from '../src/components/Contract';
+import Contract from '../src/components/contract/Contract';
 import axios from 'axios';
 
 const contractApp = <Contract />
