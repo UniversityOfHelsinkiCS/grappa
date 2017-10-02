19e3a6b763e60018bd9930646c83ac98964b0882
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-10-02 09:18:40 +0300

    fixed sisennykset

--- src/components/contract/ContractActions.js
+++ src/components/contract/ContractActions.js
@@ -3,15 +3,15 @@ export const CONTRACT_SAVE_SUCCESS = 'CONTRACT_SAVE_SUCCESS'
 export const CONTRACT_SAVE_FAILURE = 'CONTRACT_SAVE_FAILURE'
 
 export function saveSuccess() {
-  return {
-    type: CONTRACT_SAVE_FAILURE,
-    text: 'Sopimus talletettu onnistuneesti'
-  };
+    return {
+      type: CONTRACT_SAVE_FAILURE,
+      text: 'Sopimus talletettu onnistuneesti'
+    };
 }
 
 export function saveFailure() {
-  return {
-    type: CONTRACT_SAVE_FAILURE,
-    text: 'Sopimuksen talletus epäonnistui'
-  }
+    return {
+        type: CONTRACT_SAVE_FAILURE,
+        text: 'Sopimuksen talletus epäonnistui'
+    }
 }
