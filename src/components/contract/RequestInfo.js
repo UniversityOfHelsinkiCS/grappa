c577f5b5ef9f81b8e7bb784c1625c97b6975136f
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-10-02 11:09:11 +0300

    removed debug print

--- src/components/contract/RequestInfo.js
+++ src/components/contract/RequestInfo.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import contractSave from "./ContractReducer";
 
 const requestWasReturned = (event) => {
-    console.log("requestWasReturned " + event.text);
     const data = contractSave([], event);
     console.log(data);
   };
