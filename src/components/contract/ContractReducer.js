9c3aeb1e2f5bcf8f126efac008331e4c81f167ce
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-10-02 09:32:48 +0300

    function is now const and it is exported

--- src/components/contract/ContractReducer.js
+++ src/components/contract/ContractReducer.js
@@ -1,15 +1,20 @@
-import {CONTRACT_SAVE_SUCCESS, CONTRACT_SAVE_FAILURE} from ./ContractActions
+import {CONTRACT_SAVE_SUCCESS, CONTRACT_SAVE_FAILURE} from "./ContractActions"
 
 const initialState = { data: [] };
 
-export default function(state = initialState, action) {
+const contractSave = (state = initialState, action) => {
     switch(action.type) {
         case CONTRACT_SAVE_SUCCESS :
-            return data: action.text;
+            return {
+              data: action.text,
+            };
         case CONTRACT_SAVE_FAILURE :
-            return data: action.text;
-
+            return {
+              data: action.text,
+            };
      default:
          return state;
   }
-}
+};
+
+export default contractSave;
