c0d126e0b30133ee0ecba91e6e9226424b130262
Poletaev Dmitry <poletaev@da3-cs-dk110-12.pc.helsinki.fi>
2017-10-05 16:20:25 +0300

    remvoe path to deleted file

--- src/components/contract/Contract.js
+++ src/components/contract/Contract.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-//import ContractResponse from "./ContractResponse";
 
 //redux
 import { connect, subscribe } from "react-redux";
