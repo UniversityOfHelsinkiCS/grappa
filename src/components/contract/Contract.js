80d950a51247c45a30e65264b592cdf4b6b07383
Poletaev Dmitry <poletaev@da3-cs-dk110-12.pc.helsinki.fi>
2017-10-05 16:19:49 +0300

    remove unneeded file

--- src/components/contract/Contract.js
+++ src/components/contract/Contract.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import ContractResponse from "./ContractResponse";
+//import ContractResponse from "./ContractResponse";
 
 //redux
 import { connect, subscribe } from "react-redux";
