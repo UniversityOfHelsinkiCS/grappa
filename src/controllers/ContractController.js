6628fcd63a078ead646a6ca9162cd99fd01a6ecc
Terho Uotila <terho.uotila@helsinki.fi>
2017-10-08 20:23:48 +0300

    getContractById

--- src/controllers/ContractController.js
+++ src/controllers/ContractController.js
@@ -3,11 +3,9 @@ const contractDao = require('../dao/ContractDao');
 const express = require('express');
 const app = express();
 
-import { send } from '../output.js';
-
 export async function getContractById(req, res) {
     const contract = await contractDao.getContractById(req.params.id);
-    send(req.query.outputType, res.status(200), contract);
+    res.status(200).json(contract);
 }
 
 export async function saveContract(req, res) {
@@ -15,18 +13,16 @@ export async function saveContract(req, res) {
     if(contractData.contractId !== "" && contractData.contractId !== undefined){
         try{
             const daoResponse = await contractDao.updateContract(contractData);
-            send(req.query.outputType, res.status(200), {text: "update successed", contractId: daoResponse});
+            res.status(200).json({text: "contract update successful", contractId: daoResponse});
         } catch (err) {
-            send(req.query.outputType, res.status(500), {text: "error occurred", error: err});
+            res.status(500).json({text: "error occurred", error: err});
         }
     } else {
         try{
             const daoResponse = await contractDao.saveNewContract(contractData);
-            send(req.query.outputType, res.status(200), {text: "save successed", contractId: daoResponse});
+            res.status(200).json({text: "contract save successful", contractId: daoResponse});
         } catch (err) {
-            send(req.query.outputType, res.status(500), {text: "error occurred", error: err});
+            res.status(500).json({text: "error occurred", error: err});
         }
-
-
     }
 }
--- test/ContractController.spec.js
+++ test/ContractController.spec.js
@@ -1,44 +1,100 @@
 import test from 'ava';
 import sinon from 'sinon';
+import router from '../src/routes/contracts.js';
 
-const contractController = require('../src/controllers/ContractController');
-const dao = require('../src/dao/ContractDao');
-const output = require('../src/output');
-const req = {
-    query: { outputType: "json" },
-    params: { id: 124 }
-};
-const resAPI = { status: a => { return a } };
-const sendStub = sinon.stub(output, "send");
+const reqres = require('reqres');
+
+//const req = {
+//    query: { outputType: "json" },
+//    params: { id: 124 }
+//};
+
+let req;
+let res;
+let contractController;
+let dao;
+
+test.beforeEach(async t => {
+    req = reqres.req();
+    res = reqres.res();
+    dao = require('../src/dao/ContractDao');
+    contractController = require('../src/controllers/ContractController');
+});
 
 test.afterEach(async t => {
-    sendStub.reset();
 });
 
-test.serial('getContractById calls output.send() and dao.contractById()', t => {
-    const stubDao = sinon.stub(dao, "getContractById");
-    stubDao.withArgs(req.params.id).returns("ok");
-
-    const resMock = sinon.mock(resAPI);
-    const expectation1 = resMock.expects("status").once().withArgs(200).returns(200);
-    
-    contractController.getContractById(req, resAPI)
-        .then(()=>{
-            t.truthy(sendStub.calledWith([req.query.outputType, 200, "ok"]));
-            resMock.verify();
-        });
-    
+test('getContractById calls dao.contractById() once', t => {
+    const stub = sinon.stub(dao, "getContractById");
+    contractController.dao = dao;
+    contractController.getContractById(req, res);
+    t.is(stub.calledOnce, true, "contractById is called once");
+    dao.getContractById.restore();
 });
 
-test.serial('saveContract calls output.send() and dao.saveContract()', t => {
-    const stubDaoSave = sinon.stub(dao, "saveContract");
-    stubDaoSave.withArgs(req).returns("ok");
+test('getContractById calls dao.contractById() with correct id', t => {
+    const stub = sinon.stub(dao, "getContractById");
+    req.params.id = 123;
+    stub.returns("ok");
+    contractController.dao = dao;
+    contractController.getContractById(req, res);
+    t.is(stub.calledWith(req.params.id), true, "contractById is called with correct id");
+    dao.getContractById.restore();
+});
 
-    const resMock = sinon.mock(resAPI);
-    const expectation1 = resMock.expects("status").once().withArgs(200).returns(200);
-    contractController.getContractById(req, resAPI)
+test.cb('getContractById returns correct information', t => {
+    const stub = sinon.stub(dao, "getContractById");
+    req.params.id = 123;
+    stub.returns({ test: "xoxo" });
+    stub.withArgs(req.params.id).returns({ test: "ok" });
+    contractController.dao = dao;
+    contractController.getContractById(req, res)
         .then(() => {
-            t.truthy(sendStub.calledWith(req.query.outputType, 200, "ok"));
-            resMock.verify();
+            t.is(res.json.calledWith({ test: "ok" }), true, "contractById returns correct information");
+            t.end();
         });
+    dao.getContractById.restore();
 });
+
+test.cb('getContractById returns status 200', t => {
+    const stub = sinon.stub(dao, "getContractById");
+    req.params.id = 123;
+    stub.returns({ test: "xoxo" });
+    stub.withArgs(req.params.id).returns({ test: "ok" });
+    contractController.dao = dao;
+    contractController.getContractById(req, res)
+        .then(() => {
+            t.is(res.status.calledWith(200), true, "contractById returns status 200");
+            t.end();
+        });
+    dao.getContractById.restore();
+});
+
+test.cb('getContractById does not return incorrect information', t => {
+    const stub = sinon.stub(dao, "getContractById");
+    req.params.id = 123;
+    stub.returns({ test: "nok" });
+    contractController.dao = dao;
+    contractController.getContractById(req, res)
+        .then(() => {
+            t.not(res.json.calledWith({ test: "xoxo" }), true, "contractById does not return incorrect information");
+            t.end();
+        });
+    dao.getContractById.restore();
+});
+
+
+test.todo('saveContract');
+
+//test.serial('saveContract calls output.send() and dao.saveContract()', t => {
+//    const stubDaoSave = sinon.stub(dao, "saveContract");
+//    stubDaoSave.withArgs(req).returns("ok");
+
+//    const resMock = sinon.mock(resAPI);
+//    const expectation1 = resMock.expects("status").once().withArgs(200).returns(200);
+//    contractController.getContractById(req, resAPI)
+//        .then(() => {
+//            t.truthy(sendStub.calledWith(req.query.outputType, 200, "ok"));
+//            resMock.verify();
+//        });
+//});
