4287b6706e43e7a338b97329164265223599b2cb
Joonas Sarapalo <joonas.sarapalo@gmail.com>
2017-10-12 15:01:13 +0300

    Added agreement put.

--- src/routes/agreements.js
+++ src/routes/agreements.js
@@ -8,13 +8,17 @@ const agreementController = require('../controllers/AgreementController');
 // router.get('/', (req, res) => {
 //     agreementController.getAgreements(req, res);
 // });
-    
+
 router.get('/:id', (req, res) => {
     agreementController.getAgreementById(req, res);
 });
-    
+
 router.post('/', jsonParser, (req, res) => {
-        agreementController.saveAgreement(req, res);
+    agreementController.saveAgreement(req, res);
 });
-    
+
+router.put('/:id', jsonParser, (req, res) => {
+    agreementController.saveAgreement(req, res);
+});
+
 module.exports = router;
