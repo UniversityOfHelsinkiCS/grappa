db40301099525fe44bb4662fe35971724ec798d3
Joonas Sarapalo <joonas.sarapalo@gmail.com>
2017-10-14 19:03:03 +0300

    Renamed tests.

--- test/agreement/AgreementEditModalField.spec.js
+++ test/agreement/AgreementEditModalField.spec.js
@@ -10,35 +10,35 @@ const mockField = shallow(<AgreementEditModalField fieldName={ "fname" } content
 
 const mockTextarea = shallow(<AgreementEditModalField fieldName={ "tname" } content={ "tcontent" } originalContent={ "toriginal" } textField={ true } onChange={() => {}}/>);
 
-test('modal edit field has right div class', t => {
+test('agreement edit modal field has right div class', t => {
     t.truthy(mockField.find('div[className="two fields"]').length, 1);
 });
 
-test('modal edit textarea has right div class', t => {
+test('agreement edit modal textarea has right div class', t => {
     t.truthy(mockTextarea.find('div[className="field ui"]').length, 1);
 });
 
-test('modal edit textarea has label', t => {
+test('agreement edit modal textarea has label', t => {
     t.truthy(mockTextarea.find('label').length, 1);
 });
 
-test('modal edit field has input field', t => {
+test('agreement edit modal field has input field', t => {
     t.truthy(mockField.find('input').length, 1);
 });
 
-test('modal edit textarea has textarea', t => {
+test('agreement edit modal textarea has textarea', t => {
     t.truthy(mockTextarea.find('textarea').length, 1);
 });
 
-test('modal edit field has a reset button', t => {
+test('agreement edit modal field has a reset button', t => {
     t.truthy(mockField.containsMatchingElement(<button className="ui primary button">reset</button>));
 });
 
-test('modal edit textarea has a reset button', t => {
+test('agreement edit modal textarea has a reset button', t => {
     t.truthy(mockTextarea.containsMatchingElement(<button className="ui right floated primary button">reset</button>));
 });
 
-test('when field input value changes handleChange is called', t => {
+test('when agreement edit modal field input value changes handleChange is called', t => {
     const instance = mockField.instance();
     const spy = sinon.stub(instance, "handleChange");
     instance.forceUpdate();
@@ -48,7 +48,7 @@ test('when field input value changes handleChange is called', t => {
     spy.restore();
 });
 
-test('when textarea value changes handleChange is called', t => {
+test('when agreement edit modal textarea value changes handleChange is called', t => {
     const instance = mockTextarea.instance();
     const spy = sinon.stub(instance, "handleChange");
     instance.forceUpdate();
@@ -58,17 +58,17 @@ test('when textarea value changes handleChange is called', t => {
     spy.restore();
 });
 
-test('when field input value changes component state changes', t => {
+test('when agreement edit modal field input value changes component state changes', t => {
     mockField.find('input').simulate('change', {target: {value: 'a'}});
     t.truthy(mockField.unrendered.props.content, 'fcontenta');
 });
 
-test('when textarea value changes component state changes', t => {
+test('when agreement edit modal textarea value changes component state changes', t => {
     mockTextarea.find('textarea').simulate('change', {target: {value: 'a'}});
     t.truthy(mockField.unrendered.props.content, 'fcontenta');
 });
 
-test('when field reset button is clicked resetContent is called', t => {
+test('when agreement edit modal field reset button is clicked resetContent is called', t => {
     const instance = mockField.instance();
     const spy = sinon.stub(instance, "resetContent");
     instance.forceUpdate();
@@ -78,7 +78,7 @@ test('when field reset button is clicked resetContent is called', t => {
     spy.restore();
 });
 
-test('when textarea reset button is clicked resetContent is called', t => {
+test('when agreement edit modal textarea reset button is clicked resetContent is called', t => {
     const instance = mockField.instance();
     const spy = sinon.stub(instance, "resetContent");
     instance.forceUpdate();
@@ -88,12 +88,12 @@ test('when textarea reset button is clicked resetContent is called', t => {
     spy.restore();
 });
 
-test('when textarea reset button is clicked resetContent is called', t => {
+test('when agreement edit modal field reset button is clicked component state changes', t => {
     mockField.find('button').simulate('click');
     t.truthy(mockField.unrendered.props.content, 'foriginal');
 });
 
-test('when textarea value changes component state changes', t => {
+test('when agreement edit modal textarea reset button is clicked component state changes', t => {
     mockTextarea.find('textarea').simulate('click');
     t.truthy(mockField.unrendered.props.content, 'foriginal');
 });
