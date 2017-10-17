80920739c85b1c997bedaab8fa3eeed7b3b6231e
Uotila Terho T <ttuotila@cc.helsinki.fi>
2017-10-17 11:59:51 +0300

    and fix it

--- src/routes/agreements.js
+++ src/routes/agreements.js
@@ -5,7 +5,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const agreementController = require('../controllers/AgreementController');
 
 router.get('/', (req, res) => {
-    agreementController.getAllAgreements();
+    agreementController.getAllAgreements(req, res);
 });
 
 router.get('/:id', (req, res) => {
