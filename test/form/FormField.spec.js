376e315cb4f71275d0882da87cb72bb617cc1ea5
Poletaev Dmitry <poletaev@dat-13.cs.helsinki.fi>
2017-10-20 12:38:44 +0300

    FormField testing

---
+++ test/form/FormField.spec.js
@@ -0,0 +1,161 @@
+import test from 'ava';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import React, { Component } from 'react';
+import FormField from '../../src/components/form/FormField';
+require('ignore-styles')
+
+const fieldTestData = {
+    inputType: "textarea",
+    rows: 1,
+    name: "thesisWorkStudentTime",
+    label: "Opiskelijan arvioima opinnäytetyöhön käytettävä työaika",
+    extraClassNames: "fluid",
+    required: true,
+    placeholder: "(esim. tuntia / vko)"
+};
+
+
+test('correct input field returned', t => {
+    const fieldKey = 'someTestingKey';
+    const fieldData = {
+        inputType: "input",
+        name: "someTestName",
+        label: "Testing label",
+        placeholder: "test placeholder"
+    };
+    const fieldOnChangeSpy = () => {};
+    const formField = <FormField
+        fieldKey={fieldKey}
+        fieldData={fieldData}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />;
+    const wrapper = shallow(formField);
+
+    const expected = <div key={fieldKey + "fieldDiv"} className={'field small input'}>
+                        <label key={fieldKey + "label"} >{fieldData.label}</label>
+                        <input
+                            key={fieldKey}
+                            name={fieldData.name}
+                            type="text" placeholder={fieldData.placeholder}
+                            onChange={fieldOnChangeSpy} />
+                    </div>
+    
+    t.truthy(wrapper.contains(expected));
+});
+
+test('correct textarea field returned', t => {
+    const fieldKey = 'someTestingKey2';
+    const fieldData = {
+        inputType: "textarea",
+        rows: 3,
+        name: "someTestName2",
+        label: "Testing label 2",
+        placeholder: "test placeholder 2"
+    };
+    const fieldOnChangeSpy = () => {};
+    const formField = <FormField
+        fieldKey={fieldKey}
+        fieldData={fieldData}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />;
+    const wrapper = shallow(formField);
+
+    const expected = <div key={fieldKey + "fieldDiv"} className={'field small textarea'}>
+                        <label key={fieldKey + "label"}>{fieldData.label}</label>
+                        <textarea 
+                           key={fieldKey} 
+                           name={fieldData.name} 
+                           rows={fieldData.rows} 
+                           placeholder={fieldData.placeholder} 
+                           onChange={fieldOnChangeSpy} ></textarea>
+                    </div>
+    
+    t.truthy(wrapper.contains(expected));
+});
+
+test('correct bareText field returned', t => {
+    const fieldKey = 'someTestingKey3';
+    const fieldData = {
+        inputType: "bareText",
+        name: "someTestName3",
+        label: "Testing label 3",
+        placeholder: "test placeholder 3"
+    };
+    const fieldOnChangeSpy = () => {};
+    const formField = <FormField
+        fieldKey={fieldKey}
+        fieldData={fieldData}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />;
+    const wrapper = shallow(formField);
+
+    const expected = <div key={fieldKey + "fieldDiv"} className={'field small bareText'}>
+                        <label key={fieldKey + "label"} >{fieldData.label}</label>
+                        <p 
+                            key={fieldKey} 
+                            id={fieldData.name} 
+                            type="text" placeholder={fieldData.placeholder} />
+                    </div>
+    
+    t.truthy(wrapper.contains(expected));
+})
+
+test('extraClasses are added correctly', t => {
+    const fieldKey = 'someTestingKey4';
+    const fieldData = {
+        inputType: "input",
+        name: "someTestName4",
+        label: "Testing label 4",
+        placeholder: "test placeholder 4",
+        extraClassNames: "someExtraClass"
+    };
+    const fieldOnChangeSpy = () => {};
+    const formField = <FormField
+        fieldKey={fieldKey}
+        fieldData={fieldData}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />;
+    const wrapper = shallow(formField);
+
+    const expected = <div key={fieldKey + "fieldDiv"} className={'field small someExtraClass input'}>
+                        <label key={fieldKey + "label"} >{fieldData.label}</label>
+                        <input
+                            key={fieldKey}
+                            name={fieldData.name}
+                            type="text" placeholder={fieldData.placeholder}
+                            onChange={fieldOnChangeSpy} />
+                    </div>
+    
+    t.truthy(wrapper.contains(expected));
+})
+
+test('required flag is correctly translated', t => {
+    const fieldKey = 'someTestingKey5';
+    const fieldData = {
+        inputType: "input",
+        name: "someTestName5",
+        label: "Testing label 5",
+        placeholder: "test placeholder 5",
+        required: true
+    };
+    const fieldOnChangeSpy = () => {};
+    const formField = <FormField
+        fieldKey={fieldKey}
+        fieldData={fieldData}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />;
+    const wrapper = shallow(formField);
+
+    const expected = <div key={fieldKey + "fieldDiv"} className={'field small input required'}>
+                        <label key={fieldKey + "label"} >{fieldData.label}</label>
+                        <input
+                            key={fieldKey}
+                            name={fieldData.name}
+                            type="text" placeholder={fieldData.placeholder}
+                            onChange={fieldOnChangeSpy} />
+                    </div>
+    
+    t.truthy(wrapper.contains(expected));
+})
