9267ae399d3894d58a0d80c7de18fc337f84ac81
Poletaev Dmitry <poletaev@dat-13.cs.helsinki.fi>
2017-10-24 11:27:24 +0300

    tests created for FormCreator

--- test/form/FormCreator.spec.js
+++ test/form/FormCreator.spec.js
@@ -1,32 +1,98 @@
 import test from 'ava';
-import { shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
 import React, { Component } from 'react';
-import FormField from '../../src/components/form/FormCreator';
-import Section from '../../src/components/form/FormSection';
-import FormField from '../../src/components/form/FormField';
+import FormCreator from '../../src/components/form/FormCreator';
+import FormSection from '../../src/components/form/FormSection';
 
 
 require('ignore-styles')
+/*
+<FormCreator 
+    formFieldInfo={this.formFieldInfo}
+    onSubmitFunc={(e)=>{if (e !== undefined){e.preventDefault();}}} 
+    buttonOnClickFunc={this.sendForm} 
+    accessToStore={this.props.agreement}
+    fieldOnChangeFunc={this.handleFormChange} />
+*/
 
+test('Section child is created', t => {
 
-test('when element is added, it creates FormField object', t => {
-
-    const fieldData = {
-        inputType: "input",
-        name: "someTestName",
-        label: "Testing label",
-        placeholder: "test placeholder"
+    const fieldOnChangeSpy = () => { };
+    const sectionData = {
+        sections: [{
+            sectionKey: "someKey",
+            header: "Same interesting header",
+            elements: [{ fieldData: null, fieldKey: 0 }],
+            fieldOnChangeFunc: fieldOnChangeSpy
+        }]
     };
-    const fieldOnChangeSpy = () => {};
-  
-    const element = {fieldData: {fieldData}, fieldKey: 0,
-                     fieldOnChangeFunc: {fieldOnChangeSpy}};
-    const array = [element];
-    
-    const section = <Section sectionKey="Ww" header="en" elements = {array}
-                             fieldOnChangeFunc= {fieldOnChangeSpy} />
-    const wrapper = shallow(section);
-    t.is(wrapper.find(FormField).length , 1);
+
+    const form = <FormCreator
+        formFieldInfo={sectionData}
+        onSubmitFunc={() => { }}
+        buttonOnClickFunc={() => { }}
+        accessToStore={[]}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />
+
+    const wrapper = shallow(form);
+    t.is(wrapper.find(FormSection).length, 1);
+})
+
+test('Button is created and correct function is added', t => {
+
+    const fieldOnChangeSpy = () => { };
+    const buttonOnClickSpy = () => { };
+    const sectionData = {
+        sections: [{
+            sectionKey: "someKey",
+            header: "Same interesting header",
+            elements: [{ fieldData: null, fieldKey: 0 }],
+            fieldOnChangeFunc: fieldOnChangeSpy
+        }]
+    };
+
+    const form = <FormCreator
+        formFieldInfo={sectionData}
+        onSubmitFunc={() => { }}
+        buttonOnClickFunc={buttonOnClickSpy}
+        accessToStore={[]}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />
+
+    const wrapper = shallow(form);
+
+    const expected = <button className="ui primary button" type="submit" onClick={buttonOnClickSpy}>Save</button>;
+
+    t.truthy(wrapper.contains(expected));
+
+    t.is(wrapper.find('button').length, 1);
+})
+
+test('Form elemnt is created and there is only 1 element of this kind', t => {
+
+    const fieldOnChangeSpy = () => { };
+    const buttonOnClickSpy = () => { };
+    const sectionData = {
+        sections: [{
+            sectionKey: "someKey",
+            header: "Same interesting header",
+            elements: [{ fieldData: null, fieldKey: 0 }],
+            fieldOnChangeFunc: fieldOnChangeSpy
+        }]
+    };
+
+    const form = <FormCreator
+        formFieldInfo={sectionData}
+        onSubmitFunc={() => { }}
+        buttonOnClickFunc={buttonOnClickSpy}
+        accessToStore={[]}
+        fieldOnChangeFunc={fieldOnChangeSpy}
+    />
+
+    const wrapper = shallow(form);
+
+    t.is(wrapper.find('form').length, 1);
 })
