d2cdd7551ab18f16e466a150645fef9eef77c70a
Uotila Terho T <ttuotila@cc.helsinki.fi>
2017-10-24 16:17:51 +0300

    remove contract

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 const agreements = require('./routes/agreements.js');
-const contracts = require('./routes/contracts.js');
 const theses = require('./routes/theses.js');
 const index = require('./routes/index.js');
 const app = require('../index.js');
@@ -8,8 +7,6 @@ const supervisors = require('./routes/supervisors.js');
 module.exports = (app) => {
 
     app.use('/', index);
-    app.use('/contract', contracts);
-    app.use('/contracts', contracts);
     app.use('/agreements', agreements);
     app.use('/theses', theses);
     app.use('/supervisors', supervisors);
