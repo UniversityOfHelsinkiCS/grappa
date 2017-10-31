b1841ca09a9d0b7fc36f0ab865757e139833b746
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-10-31 11:48:20 +0200

    now updated migration

--- migrations/20171031111138_add_missing_columns_to_agreement.js
+++ migrations/20171031111138_add_missing_columns_to_agreement.js
@@ -1,8 +1,28 @@
+exports.up = function (knex, Promise) {
+    return Promise.all([
 
-exports.up = function(knex, Promise) {
-  
+        knex.schema.createTable('agreement', function (table) {
+            table.increments('agreementId').primary();
+            table.integer('authorId').unsigned(); //author
+            table.foreign('authorId').references('person.personId');
+            table.integer('thesisId').unsigned();
+            table.foreign('thesisId').references('thesis.thesisId');
+            table.integer('responsibleSupervisorId').unsigned();
+            table.foreign('responsibleSupervisorId').references('personRoleField.personRoleId');
+            table.integer('studyFieldId').unsigned();
+            table.foreign('studyFieldId').references('studyfield.studyfieldId');
+            table.boolean('fake');
+            table.integer('gradeGoal');
+            table.string('studentWorkTime');
+            table.string('supervisorWorkTime');
+            table.string('intermediateGoal');
+            table.string('meetingAgreement');
+            table.string('other');
+            table.timestamps();
+        })
+    ]);
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    knex.schema.dropTable('agreement');
 };
