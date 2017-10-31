8e8a47e18f4d52fb9c891a22edd6a05bb6ed5bf1
Jami Kousa <jakousa@cs.helsinki.fi>
2017-10-31 13:05:31 +0200

    add thesis to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,19 +9,19 @@ export default class NavBar extends Component {
             {path: '/agreement', text: 'Agreement'},
             {path: '/theses', text: 'Theses'},
             {path: '/graderManagement', text: 'Supervisor management'},
+            {path: '/thesis', text: 'New thesis'}
         ]
-        const linkElements = elements.map(elem => {
-            if(this.props.active === elem.text)
-                return <Link key={elem.text} to={elem.path} className="item active">{elem.text}</Link>;
-            else
-                return <Link key={elem.text} to={elem.path} className="item">{elem.text}</Link>
-         });
+        const linkElements = elements.map((elem, index) => 
+            this.props.active === elem.text ? 
+                <Link key={index} to={elem.path} className="item active">{elem.text}</Link> :
+                <Link key={index} to={elem.path} className="item">{elem.text}</Link>
+         );
 
         return (
             <div className="ui stackable secondary pointing menu">
                 {linkElements}
                 <div className="right menu">
-                    <Link to="/" className="item"> Logout </Link>
+                    <Link to="/" className="item">Logout</Link>
                 </div>
             </div>
         );
