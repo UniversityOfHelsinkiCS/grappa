fe739e6528f062b7fffaf40e1b29ec6900849a04
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-10-31 12:37:16 +0200

    added method to save thesis

--- src/controllers/ThesisController.js
+++ src/controllers/ThesisController.js
@@ -12,3 +12,13 @@ export async function getThesisById(req, res) {
     const thesis = await thesisService.getThesisById(req.params.id);
     res.status(200).json(thesis);
 }
+
+export async function saveThesis(req, res) {
+    const agreementData = req.body;
+        try {
+            const daoResponse = await agreementService.saveThesis(agreementData);
+            res.status(200).json({ text: "agreement update successful", agreementId: daoResponse });
+        } catch (err) {
+            res.status(500).json({ text: "error occurred", error: err });
+        }
+}
