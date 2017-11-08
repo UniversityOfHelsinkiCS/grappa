f47c1870e0540f546ec6502acf0b90c8992804b3
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-08 13:41:17 +0200

    added catch to test

--- test/ThesisController.spec.js
+++ test/ThesisController.spec.js
@@ -62,7 +62,12 @@ test.cb.only('saveAgreement returns 500 if there no database ', t => {
     req.body = body;
     thesisController.saveThesis(req, res)
         .then(() => {
-            console.log("res status " + res.status.calledOnce);
+
+            console.log("RES THEN" + JSON.stringify(res));
+            t.is(res.status.calledWith(500), true, "saveThesis returns status 500 if no database");
+            t.end();
+        }).catch(() => {
+            console.log("res CATCH " + JSON.stringify(res));
             t.is(res.status.calledWith(500), true, "saveThesis returns status 500 if no database");
             t.end();
         });
