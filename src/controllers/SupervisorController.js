811db26fb1a802119365235ea131a7c2e1fc5bf8
Arkiherttua <ainorosah@gmail.com>
2017-11-17 20:36:39 +0200

    update supervisor finally working

--- src/controllers/SupervisorController.js
+++ src/controllers/SupervisorController.js
@@ -46,43 +46,11 @@ export async function saveSupervisor(req, res) {
         } catch (err) {
             res.status(500).json({ text: "error occurred", error: err });
         }
-    } else { //update doesn't work
-        try {
-            const supervisorRoleId = await supervisorService.getSupervisorRoleId();
-            const personData = {
-                personId: supervisorData.personId,
-                firstname: supervisorData.firstname,
-                lastname: supervisorData.lastname,
-                title: supervisorData.title,
-                email: supervisorData.email,
-                shibbolethId: supervisorData.shibbolethId,
-                isRetired: supervisorData.isRetired
-            };
-            const personRoleData = {
-                personId: supervisorData.personId,
-                studyfieldId: supervisorData.studyfieldId,
-                roleId: supervisorRoleId
-            };
-            const personRoleId = await personService.savePersonRole(personRoleData);
-            const agreementPersonData = {
-                agreementId: supervisorData.agreementId,
-                personRoleId: personRoleId,
-                roleId: supervisorRoleId,
-                approved: false,
-                statement: ''
-            };
-            
-            const agreementId = await supervisorService.updateAgreementPerson(agreementPersonData);
-            res.status(200).json({ text: "supervisor save successful", personId: personId });
-        } catch (err) {
-            res.status(500).json({ text: "error occurred", error: err });
-        }
     }
 }
 
 export async function reviewSupervisor(req, res) {
     let data = req.body;
-    //console.log("supervisorcontr, req: ", data);
     if (data.personRoleId != null && data.agreementId != null) {
         try {
             let agreementPersonData = {
@@ -102,4 +70,36 @@ export async function reviewSupervisor(req, res) {
         res.status(500).json({ text: "agreementId and personRoleId are required" });
     }
 
+}
+
+export async function updateSupervisor(req, res) {
+    const data = req.body;
+    const personData = {
+        personId: data.personId,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        shibbolethId: data.shibbolethId,
+        email: data.email,
+        title: data.title,
+        isRetired: data.isRetired
+    };
+    
+    if (personData.personId != null || personData.personId != '') {
+            let updateData = {};
+            // remove useless keys from data
+            Object.keys(personData).map(key => {
+                if (personData[key] != null) {
+                    updateData[key] = personData[key];
+                }
+            });
+            let personId = await personService.updatePerson(updateData).then((response) =>  {
+                res.status(200).json("person updated succesfully " + response);
+            }
+            ).catch(err => {
+                res.status(500).json(err);
+            });
+        } 
+     else {
+        res.status(500).json({text: "person does not exist"});
+    }
 }
