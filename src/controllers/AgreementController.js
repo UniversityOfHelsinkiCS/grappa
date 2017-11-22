3fe25fad4969df38d99f9712e97c2bde6286b661
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-11-22 12:14:25 +0200

    getagreement return only agreement

--- src/controllers/AgreementController.js
+++ src/controllers/AgreementController.js
@@ -10,7 +10,7 @@ const AttachmentController = require('./AttachmentController');
 export async function getAgreementById(req, res) {
     const agreement = await agreementService.getAgreementById(req.params.id);
     const agreementPersons = await personService.getAgreementPersonsByAgreementId(req.params.id);
-    res.status(200).json({ agreement: agreement, persons: agreementPersons });
+    res.status(200).json(agreement);
 }
 
 export async function getPreviousAgreementById(req, res) {
@@ -81,9 +81,11 @@ export async function saveAgreement(req, res) {
             agreementData.agreementId = agreementSaveResponse;
             //emailService.agreementCreated(Object.assign(personData, thesisData, agreementData)); //says atm: Unhandled rejection TypeError: Cannot read property 'email' of undefined 
             //AttachmentController.saveAttachment(req, res);
-            res.status(200).json({ person: personData, thesis: thesisData, agreement: agreementData });
+            res.status(200).json(agreementData);
+            console.log("palautan 200");
         }
         catch(error) {
+            console.log("ERROR",error);
             res.status(500).json({ text: "Error occured" });
         }
     } else {
