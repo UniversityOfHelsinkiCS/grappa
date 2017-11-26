942d0bfa91c4269ce5fc95c21cccac80fad6d688
Jakousa <jakousa@cs.helsinki.fi>
2017-11-26 03:05:33 +0200

    added some functionality for persons regarding roles

--- src/routes/shibboleth.js
+++ src/routes/shibboleth.js
@@ -2,11 +2,15 @@ const bodyParser = require('body-parser');
 const router = require('express').Router();
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const loginController = require('../controllers/LoginController');
 
+//For now we use post to login.
+router.get('/', jsonParser, (req, res) => {
+    loginController.login(req, res);
+});
 
-router.get('/',  (req, res) => {
-    console.log(req.headers);
-    res.json({ text: "Check console.log"} );
+router.get('/:id', jsonParser, (req, res) => {
+    loginController.fakeLogin(req, res);
 });
 
 module.exports = router;
