253462215a2f1ad2b88432199139f0ea470cfd59
Arkiherttua <ainorosah@gmail.com>
2017-11-26 19:36:24 +0200

    quick fix so that roles work in front end

--- seeds/03_role_seed.js
+++ seeds/03_role_seed.js
@@ -1,25 +1,29 @@
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('role').del()
-      .then(function () {
-          // Inserts seed entries
-          return knex('role').insert([
-              {
-                  roleId: 1,
-                  name: 'supervisor'
-              },
-              {
-                  roleId: 2,
-                  name: 'grader'
-              },
-              {
-                  roleId: 3,
-                  name: 'author'
-              },
-              {
-                  roleId: 4,
-                  name: 'other'
-              }
-          ]);
-      });
+    // Deletes ALL existing entries
+    return knex('role').del()
+        .then(function () {
+            // Inserts seed entries
+            return knex('role').insert([
+                {
+                    roleId: 1,
+                    name: 'supervisor'
+                },
+                {
+                    roleId: 2,
+                    name: 'grader'
+                },
+                {
+                    roleId: 3,
+                    name: 'author'
+                },
+                {
+                    roleId: 4,
+                    name: 'resp_professor'
+                },
+                {
+                    roleId: 5,
+                    name: 'other'
+                }
+            ]);
+        });
 };
--- seeds/06_personWithRole_seed.js
+++ seeds/06_personWithRole_seed.js
@@ -5,22 +5,39 @@ exports.seed = function (knex, Promise) {
             // Inserts seed entries
             return knex('personWithRole').insert([
                 {
+                    //olli ohjaaja
                     personRoleId: 1,
                     personId: 1,
                     roleId: 1,
                     studyfieldId: 1,
                 },
                 {
+                    //venla vastuuproffa
                     personRoleId: 2,
                     personId: 2,
-                    roleId: 1,
+                    roleId: 4,
                     studyfieldId: 2
                 },
                 {
+                    //anna puu muu ohjaaja
                     personRoleId: 3,
                     personId: 3,
                     roleId: 1,
                     studyfieldId: 1
+                },
+                {
+                    //erkki eläkeläinen
+                    personRoleId: 4,
+                    personId: 4,
+                    roleId: 5,
+                    studyfieldId: 1
+                },
+                {
+                    //olivia opiskelija
+                    personRoleId: 5,
+                    personId: 5,
+                    roleId: 3,
+                    studyfieldId: 2
                 }
             ]);
         });
