81343c8e2e6eff780cbcfec92f630754ef642d09
Jakousa <jakousa@cs.helsinki.fi>
2017-11-27 12:39:19 +0200

    redo permissions

--- src/util/rolePermissions.js
+++ src/util/rolePermissions.js
@@ -6,49 +6,9 @@ export const getPermissions = (role, context, method) => {
     return permissions[role][context][method];
 }
 
-export const userRoles = ['student', 'supervisor', 'resp_professor', "other_supervisor", 'admin']
+export const userRoles = ['admin', 'manager', 'print_person', 'resp_professor', 'grader', 'supervisor', 'student']
 
 const permissions = {
-    //Supervisor
-    'supervisor': {
-        'nav-bar': {
-            show: [
-                nav['home'], nav['agreement'], nav['theses'], nav['assesment']
-            ]
-        },
-        'agreement': {
-            'edit': ['thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisSupervisorSecond', 'thesisSupervisorOther', 'thesisWorkSupervisorTime', 'thesisWorkMeetingAgreement', 'thesisWorkOther']
-        }
-    },
-    //Other Supervisor
-    'other': {
-        'nav-bar': {
-            show: [
-                nav['home'], nav['agreement'], nav['theses'], nav['assesment']
-            ]
-        }
-    },
-    //Student
-    'author': {
-        'nav-bar': {
-            show: [
-                nav['home'], nav['agreement'], nav['thesis'], nav['assesment']
-            ]
-        },
-        'agreement': {
-            'create': ['studentAddress', 'studentEmail', 'studentAddress', 'studentName', 'studentPhone', 'thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisWorkOther'],
-            'edit': ['thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisSupervisorPrimary', 'thesisSupervisorSecond', 'thesisSupervisorOther', 'thesisWorkSupervisorTime', 'thesisWorkMeetingAgreement', 'thesisWorkOther']
-        }
-    },
-    //Responsible Professor
-    'resp_professor': {
-        'nav-bar': {
-            show: [
-                nav['home'], nav['agreement'], nav['theses'], nav['graderManagement'], nav['assesment']
-            ]
-        }
-    },
-    //Admin
     'admin': {
         'nav-bar': {
             show: [
@@ -59,5 +19,59 @@ const permissions = {
             'create': ['studentAddress', 'studentEmail', 'studentAddress', 'studentName', 'studentPhone', 'thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisWorkOther'],
             'edit': ['thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisSupervisorPrimary', 'thesisSupervisorSecond', 'thesisSupervisorOther', 'thesisWorkSupervisorTime', 'thesisWorkMeetingAgreement', 'thesisWorkOther']
         }
-    }
+    },
+    'manager': {
+        'nav-bar': {
+            show: [
+                nav.home, nav.agreement, nav.theses, nav.graderManagement, nav.thesis, nav.assesment, nav.councilMeeting, nav.councilMeetings, nav.emailDrafts, nav.statistics
+            ]
+        },
+        'agreement': {
+            'create': ['studentAddress', 'studentEmail', 'studentAddress', 'studentName', 'studentPhone', 'thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisWorkOther'],
+            'edit': ['thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisSupervisorPrimary', 'thesisSupervisorSecond', 'thesisSupervisorOther', 'thesisWorkSupervisorTime', 'thesisWorkMeetingAgreement', 'thesisWorkOther']
+        }
+    },
+    'print_person': {
+        'nav-bar': {
+            show: [
+                nav['home'], nav['agreement'], nav['theses'], nav['assesment']
+            ]
+        },
+        'agreement': {
+            'edit': ['thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisSupervisorSecond', 'thesisSupervisorOther', 'thesisWorkSupervisorTime', 'thesisWorkMeetingAgreement', 'thesisWorkOther']
+        }
+    },
+    'resp_professor': {
+        'nav-bar': {
+            show: [
+                nav['home'], nav['agreement'], nav['theses'], nav['graderManagement'], nav['assesment']
+            ]
+        }
+    },
+    'grader': {
+        'nav-bar': {
+            show: [
+                nav['home'], nav['agreement'], nav['theses'], nav['assesment']
+            ]
+        }
+    },
+    'supervisor': {
+        'nav-bar': {
+            show: [
+                nav['home'], nav['agreement'], nav['theses'], nav['assesment']
+            ]
+        }
+    },
+    //Student
+    'student': {
+        'nav-bar': {
+            show: [
+                nav['home'], nav['agreement'], nav['thesis'], nav['assesment']
+            ]
+        },
+        'agreement': {
+            'create': ['studentAddress', 'studentEmail', 'studentAddress', 'studentName', 'studentPhone', 'thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisWorkOther'],
+            'edit': ['thesisCompletionEta', 'thesisWorkStudentTime', 'thesisWorkIntermediateGoal', 'thesisWorkMeetingAgreement', 'thesisSupervisorPrimary', 'thesisSupervisorSecond', 'thesisSupervisorOther', 'thesisWorkSupervisorTime', 'thesisWorkMeetingAgreement', 'thesisWorkOther']
+        }
+    },
 }
