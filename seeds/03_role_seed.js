70df0ebafdb7ad736e1d9e2d01d269d0fc69bd13
Jakousa <jakousa@cs.helsinki.fi>
2017-11-27 12:39:34 +0200

    added cases

--- seeds/03_role_seed.js
+++ seeds/03_role_seed.js
@@ -4,25 +4,39 @@ exports.seed = function (knex, Promise) {
         .then(function () {
             // Inserts seed entries
             return knex('role').insert([
+                // Roles are ordered by implicit data access rights.
                 {
                     roleId: 1,
-                    name: 'supervisor'
+                    name: 'admin' 
+                    // Admin means you and I
                 },
                 {
                     roleId: 2,
-                    name: 'grader'
+                    name: 'manager' 
+                    // Manager is person(s) resposible for a studyfield management
+                    // Such as opintoesimies. Schedules councilmeetings etc.
                 },
                 {
                     roleId: 3,
-                    name: 'author'
+                    name: 'print_person'
+                    // Print-person is a manager also, but only responsible
+                    // for printing / viewing data
                 },
                 {
                     roleId: 4,
                     name: 'resp_professor'
+                    // Responsible professor is in charge of a studyfield.
+                    // They decide who is approved as a supervisor and/or grader
                 },
                 {
                     roleId: 5,
-                    name: 'other'
+                    name: 'grader'
+                    // Grader is responsible for grading a student
+                },
+                {
+                    roleId: 6,
+                    name: 'supervisor'
+                    // Supervisor is working to assist and supervise the student
                 }
             ]);
         });
--- seeds/06_personWithRole_seed.js
+++ seeds/06_personWithRole_seed.js
@@ -4,40 +4,73 @@ exports.seed = function (knex, Promise) {
         .then(function () {
             // Inserts seed entries
             return knex('personWithRole').insert([
+                //Range of ordinary personRoleIds, personIds and roleIds from 1 to 6
                 {
-                    //olli ohjaaja
-                    personRoleId: 1,
-                    personId: 1,
-                    roleId: 1,
-                    studyfieldId: 1,
+                    personRoleId: 1, 
+                    personId: 1, // Amanda Admin
+                    roleId: 1, // admin
+                    studyfieldId: 1 // Irrelevant for admin, Tietojenkäsittelytiede
                 },
                 {
-                    //venla vastuuproffa
-                    personRoleId: 2,
-                    personId: 2,
-                    roleId: 4,
-                    studyfieldId: 2
+                    personRoleId: 2, 
+                    personId: 2, // Harri CS-Hallinto
+                    roleId: 2, // manager
+                    studyfieldId: 1 // Tietojenkäsittelytiede
                 },
                 {
-                    //anna puu muu ohjaaja
-                    personRoleId: 3,
-                    personId: 3,
-                    roleId: 1,
-                    studyfieldId: 1
+                    personRoleId: 3, 
+                    personId: 3, // Petra CS-Printtaaja
+                    roleId: 3, // print-person
+                    studyfieldId: 1 // Tietojenkäsittelytiede
                 },
                 {
-                    //erkki eläkeläinen
-                    personRoleId: 4,
-                    personId: 4,
-                    roleId: 5,
-                    studyfieldId: 1
+                    personRoleId: 4, 
+                    personId: 4, // Victoria CS-Vastuuproffa
+                    roleId: 4, // resp_professor
+                    studyfieldId: 1 // Tietojenkäsittelytiede
                 },
                 {
-                    //olivia opiskelija
                     personRoleId: 5,
-                    personId: 5,
-                    roleId: 3,
-                    studyfieldId: 2
+                    personId: 5, // Thomas CS-Tarkastaja
+                    roleId: 5, // grader
+                    studyfieldId: 1 // Tietojenkäsittelytiede
+                },
+                {
+                    personRoleId: 6,
+                    personId: 6, // Olli CS-Ohjaaja
+                    roleId: 6, // supervisor 
+                    studyfieldId: 1 // Tietojenkäsittelytiede
+                },
+                //Defaults ended here, now special people:
+                {
+                    personRoleId: 8,
+                    personId: 8, // Venla Math-vastuuproffa
+                    roleId: 4, // resp_professor
+                    studyfieldId: 2 // Matematiikka
+                },
+                {
+                    personRoleId: 9,
+                    personId: 9, // Anna Puu
+                    roleId: 6, // supervisor
+                    studyfieldId: 1 // Tietojenkäsittelytiede
+                },
+                {
+                    personRoleId: 10,
+                    personId: 9, // Anna Puu
+                    roleId: 6, // supervisor
+                    studyfieldId: 2 // Matematiikka
+                },
+                {
+                    personRoleId: 13,
+                    personId: 11, // Erkki Erikoistapaus
+                    roleId: 3, // print-person
+                    studyfieldId: 2 // Matematiikka
+                },
+                {
+                    personRoleId: 15,
+                    personId: 11, // Erkki Erikoistapaus
+                    roleId: 4, // supervisor
+                    studyfieldId: 1 // Tietojenkäsittelytiede
                 }
             ]);
         });
