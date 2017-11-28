a18c1740ec793edde9598d5f1eeca4ef2d8d2d22
welxo88 <welxo88@gmail.com>
2017-11-28 11:25:20 +0200

    xmlbuilder install + ethesius further dev

--- src/util/ethesis.js
+++ src/util/ethesis.js
@@ -1,5 +1,33 @@
 import axios from 'axios'
+import xml from 'xmlbuilder'
+import creds from './ethesis_credentials'
 
 export async function saveToEThesis(meta, pdf){
+
+    const config = { headers: { 'auth': creds } };
+
+    var doc = xml.create();
     
+    doc.begin('root')
+      .ele('xmlbuilder')
+        .att('for', 'node-js')
+        .ele('repo')
+          .att('type', 'git')
+          .txt('git://github.com/oozcitak/xmlbuilder-js.git') 
+        .up()
+      .up()
+      .ele('test')
+        .txt('complete');
+    
+    console.log(doc.toString({ pretty: true }));
+
+
+    const data = new FormData();
+    data.append('action', 'ADD');
+    data.append('file', new Blob(['test payload'], { type: 'text/csv' }));
+
+    axios.post('http://kirjasto-test.hulib.helsinki.fi/ethesis-swordv2/collection/123456789/13', data, config)
+    .then(response => console.log(response))
+    .catch(errors => console.log(errors));
+
 }
