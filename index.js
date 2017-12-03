8a8c8b4226dfe969edc476bb225d072c246929af
Terho Uotila <terho.uotila@helsinki.fi>
2017-12-04 01:43:56 +0200

    save session data through knex

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express();
 const routes = require('./src/routes.js');
 const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const knex = require('./src/db/connection.js');
+
+const store = new KnexSessionStore({
+  knex: knex,
+  tablename: 'sessions' // optional. Defaults to 'sessions'
+});
 
 module.exports = app;
 
@@ -12,7 +19,7 @@ app.listen(3100, () => {
   console.log('Example app listening on port 3100!');
 })
 
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, store: store, resave: false, saveUninitialized: false}))
 
 routes(app);
 
