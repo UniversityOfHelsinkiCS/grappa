dc28002f853f17e87c73ec6db517c4454d6f2ea3
Terho Uotila <terho.uotila@helsinki.fi>
2017-12-04 01:45:00 +0200

    add shibboleth users to db

--- src/routes.js
+++ src/routes.js
@@ -13,10 +13,11 @@ const auth = require('./middleware/auth');
 
 
 module.exports = (app) => {
+    app.use(auth.shibRegister);
     app.use('/', index);
     app.use('/login', shibboleth)
     app.use('/persons', persons);
-    app.use(auth.authenticate);    
+    app.use(auth.checkAuth);
     app.use('/agreements', agreements);
     app.use('/theses', theses);
     app.use('/supervisors', supervisors);
