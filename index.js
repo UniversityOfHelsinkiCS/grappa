70484ffe6ae78cedcae5178e341e0c9870bf1889
Terho Uotila <terho.uotila@helsinki.fi>
2017-12-04 00:21:17 +0200

    give stack trace for unhandled promise rejection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('babel-polyfill');
 const express = require('express');
 const app = express();
 const routes = require('./src/routes.js');
+const session = require('express-session');
 
 module.exports = app;
 
@@ -11,7 +12,15 @@ app.listen(3100, () => {
   console.log('Example app listening on port 3100!');
 })
 
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
 routes(app);
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'stack:', reason);
+  // application specific logging, throwing an error, or other logic here
+  console.log(reason.stack);
+});
 
 process.on('SIGTERM', function () {
   server.close(function () {
