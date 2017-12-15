f432bfa8d5a578219c932a853cfb17bb7125eec5
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-12-15 11:30:28 +0200

    plugin added

--- src/db/bookshelf.js
+++ src/db/bookshelf.js
@@ -1,5 +1,8 @@
 'use strict';
 const knex = require('./connection')
 const bookshelf = require('bookshelf')(knex);
+bookshelf.plugin('bookshelf-validate', {
+    validator: validator
+  });
 
 module.exports = bookshelf;
