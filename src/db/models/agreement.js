7166fe5e0498f7fcfa07171b79ed88fed6732aa6
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-12-15 11:30:11 +0200

    trying to validate, dimi plz help

--- src/db/models/agreement.js
+++ src/db/models/agreement.js
@@ -1,6 +1,11 @@
 'use strict';
 const bookshelf = require('../bookshelf');
 const Agreement = bookshelf.Model.extend({
+    validations: {
+        studentWorkTime: {
+          isRequired: true,
+          isLength: [1, 100] // will call validator.isLength(value, 2, 32) 
+        }},
     tableName: 'agreement',
     idAttribute: 'agreementId',
 });
