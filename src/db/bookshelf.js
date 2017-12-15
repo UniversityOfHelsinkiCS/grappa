c61ba47c4273b6aae2464ec0a2323551c22e28a3
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-12-15 13:50:11 +0200

    no more bookshelf validate

--- src/db/bookshelf.js
+++ src/db/bookshelf.js
@@ -1,9 +1,5 @@
 'use strict';
 const knex = require('./connection')
 const bookshelf = require('bookshelf')(knex);
-bookshelf.plugin('bookshelf-validate',{
-  validator: require('validator'), // node-validator
-  validateOnSave: true // Automatically validate when Bookshelf emits 'saving' event
-});
 
 module.exports = bookshelf;
