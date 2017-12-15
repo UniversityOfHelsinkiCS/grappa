3de1bdb4dd013fb53a59636edc8ab0f4934db3be
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-12-15 13:50:24 +0200

    no more validations

--- src/db/models/agreement.js
+++ src/db/models/agreement.js
@@ -1,10 +1,6 @@
 'use strict';
 const bookshelf = require('../bookshelf');
 const Agreement = bookshelf.Model.extend({
-    validations: {
-        studentWorkTime: 'isRequired',
-        error: 'You must spesify how much work you intend to put in.',
-      },
     tableName: 'agreement',
     idAttribute: 'agreementId',
 });
