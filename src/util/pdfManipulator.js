81199cc4cd8e265965b439a9bad450dcc7170748
Jakousa <jakousa@cs.helsinki.fi>
2018-01-03 14:48:15 +0200

    downloading attachments

---
+++ src/util/pdfManipulator.js
@@ -0,0 +1,24 @@
+const pdftk = require('node-pdftk');
+
+/**
+ * Joins the PDF-files into a single file
+ * @param {String} pathToFolder - Absolute path to the file.
+ * @param {Array<String>} fileNames - Array of files to be joined.
+ * @return {Promise<String>} Promise of the absolute path to the created file.
+ */
+export async function joinPdfs(pathToFolder, fileNames, resultFileName) {
+    //Does not support past 26.
+    let pdfs = fileNames.reduce((accumulated, current, index) => {
+        accumulated += " " + String.fromCharCode(65 + index);
+        return accumulated;
+    }, "")
+
+    const input = {};
+    fileNames.forEach((name, index) => {
+        input[String.fromCharCode(65 + index)] = pathToFolder + name;
+    })
+    return pdftk
+        .input(input)
+        .cat(pdfs)
+        .output()
+}
