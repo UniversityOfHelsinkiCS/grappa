7e795b9104762fc981f3c5fc65fefe49782b9ad9
Jakousa <jakousa@cs.helsinki.fi>
2018-01-09 15:59:09 +0200

    attachment labels

--- src/components/attachment/AttachmentList.js
+++ src/components/attachment/AttachmentList.js
@@ -10,12 +10,26 @@ export default class AttachmentList extends Component {
         this.props.deleteAttachment(attachmentId);
     }
 
+    labelToText = (label) => {
+        switch (label) {
+            case 'otherFile':
+                return 'Other'
+            case 'reviewFile':
+                return 'Review'
+            case 'thesisFile':
+                return 'Thesis'
+            default:
+                return 'Label not handled'
+        }
+    }
+
     render() {
         return (
             <table className="ui celled table">
                 <thead>
                     <tr>
                         <th>Filename</th>
+                        <th>Label</th>
                         <th>Download</th>
                         <th>Delete</th>
                     </tr>
@@ -24,6 +38,7 @@ export default class AttachmentList extends Component {
                     {this.props.attachments.map(attachment =>
                         <tr key={attachment.attachmentId}>
                             <td>{attachment.filename}</td>
+                            <td>{this.labelToText(attachment.label)}</td>
                             <td><button className="ui primary button" onClick={this.download(attachment.attachmentId)}>Download</button></td>
                             <td><button className="ui primary button" onClick={this.delete(attachment.attachmentId)}>Delete</button></td>
                         </tr>
