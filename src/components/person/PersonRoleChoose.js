8ceb6b17286ab55e35cb2491db2e857d41c930e5
Jakousa <jakousa@cs.helsinki.fi>
2018-01-10 13:44:53 +0200

    role save

--- src/components/person/PersonRoleChoose.js
+++ src/components/person/PersonRoleChoose.js
@@ -5,14 +5,108 @@ export default class PersonRoleChoose extends Component {
     constructor() {
         super();
         this.state = {
-            email: undefined
+            roleId: undefined,
+            studyfieldId: undefined,
         };
     }
 
-    render() {
-        const mail = this.state.email;
+    changeStudyfield = (event) => {
+        const studyfieldId = event.target.value;
+        this.setState({ studyfieldId });
+    }
+
+    changeRole = (event) => {
+        const roleId = event.target.value;
+        this.setState({ roleId })
+    }
+
+    studyfieldDropdown = () => {
         return (
-            <div> TODO: Control users {mail} </div>
+            <select
+                className="ui dropdown"
+                onChange={this.changeStudyfield}>
+                <option key="0" value="">Select</option>
+                {this.props.studyfields.map(field =>
+                    <option key={field.studyfieldId} value={field.studyfieldId}>
+                        {field.name}
+                    </option>
+                )}
+            </select>
+        )
+    }
+
+    roleDropdown = () => {
+        return (
+            <select
+                className="ui dropdown"
+                onChange={this.changeRole}>
+                <option key="0" value="">Select</option>
+                {this.props.availableRoles.map(role =>
+                    <option key={role.roleId} value={role.roleId}>
+                        {role.name}
+                    </option>
+                )}
+            </select>
+        )
+    }
+
+    addRole = () => {
+        const role = {
+            roleId: this.state.roleId,
+            studyfieldId: this.state.studyfieldId
+        }
+        this.props.addRole(role)
+    }
+
+    removeRole = (role) => () => {
+        this.props.removeRole(role);
+    }
+
+    render() {
+        const person = this.props.person;
+        const roles = this.props.roles;
+        return (
+            <div>
+                <h2>{person.firstname + ' ' + person.lastname}</h2>
+                <p>
+                    Removing grader is only possible if the person has not graded anything and is not grading anything.
+                    Removing supervisor is only possible if the person has not supervised anything and is not supervising anything.
+                    {/*TODO: Make sure this is the way it's supposed to be */}
+                </p>
+                <table className="ui celled table">
+                    <thead>
+                        <tr>
+                            <th>Role</th>
+                            <th>Studyfield</th>
+                            <th>Control</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {roles.map(role =>
+                            <tr key={role.personRoleId}>
+                                <td>{role.name}</td>
+                                <td>{role.studyfield}</td>
+                                <td>
+                                    {!role.agreementId ? <button className="ui red icon button"
+                                        onClick={this.removeRole(role)}>
+                                        Remove <i className="remove icon" />
+                                    </button> : 'Linked to agreement'}
+                                </td>
+                            </tr>)
+                        }
+                        <tr>
+                            <td>{this.roleDropdown()}</td>
+                            <td>{this.studyfieldDropdown()}</td>
+                            <td>
+                                <button className="ui green icon button"
+                                    onClick={this.addRole}>
+                                    Add <i className="plus icon" />
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
--- src/containers/person/PersonRoleManagePage.js
+++ src/containers/person/PersonRoleManagePage.js
@@ -5,25 +5,64 @@ import PersonSelector from '../../components/person/PersonSelector';
 import PersonInviter from '../../components/person/PersonInviter';
 import PersonRoleChoose from '../../components/person/PersonRoleChoose';
 
+import { getAvailableRoles, saveRole, deleteRole } from '../role/roleActions';
 
 export class PersonRoleManagePage extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.props.getAvailableRoles();
         this.state = {
-            person: undefined
+            person: undefined,
+            roles: undefined
         };
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (this.state.person) {
+            console.log("WTF");
+            const person = this.state.person
+            const roles = newProps.roles.filter(role => role.personId === person.personId)
+                .map(role => {
+                    role.studyfield = newProps.studyfields.find(field => field.studyfieldId === role.studyfieldId).name
+                    return role;
+                })
+            console.log(roles);
+            this.setState({ roles });
+        }
     }
 
     selectPerson = persons => {
         const person = persons.find(item => !this.state.person || item.personId !== this.state.person.personId)
-        this.setState({ person });
+        const roles = person ?
+            this.props.roles.filter(role => role.personId === person.personId)
+                .map(role => {
+                    role.studyfield = this.props.studyfields.find(field => field.studyfieldId === role.studyfieldId).name
+                    return role;
+                })
+            : undefined
+        this.setState({ person, roles });
+    }
+
+    handleAddRole = role => {
+        role.personId = this.state.person.personId;
+        this.props.saveRole(role)
+    }
+
+    handleRemoveRole = role => {
+        this.props.deleteRole(role)
     }
 
     renderManagement = () => {
         if (!this.state.person) {
             return <PersonInviter />
         }
-        return <PersonRoleChoose />
+        return <PersonRoleChoose
+            person={this.state.person}
+            roles={this.state.roles}
+            studyfields={this.props.studyfields}
+            availableRoles={this.props.availableRoles}
+            addRole={this.handleAddRole}
+            removeRole={this.handleRemoveRole} />
     }
 
     render() {
@@ -41,12 +80,23 @@ export class PersonRoleManagePage extends Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
+    getAvailableRoles() {
+        dispatch(getAvailableRoles());
+    },
+    saveRole(role) {
+        dispatch(saveRole(role))
+    },
+    deleteRole() {
+        dispatch(deleteRole())
+    },
 });
 
 const mapStateToProps = (state) => {
     return {
+        studyfields: state.studyfields,
         persons: state.persons,
         roles: state.roles,
+        availableRoles: state.availableRoles,
     };
 }
 
