4fd6cac78433b5eaf3738dd4be1358bb2eddfb83
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-01-16 15:41:44 +0200

    Update lint.

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'airbnb-base',
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        'comma-dangle': ['warn', 'never'],
+        'max-len': ['warn', 120]
+    }
+};
