2f6d4e3386978fba22cce9324b4b5c5a0e72b597
Jakousa <jakousa@cs.helsinki.fi>
2018-01-18 11:11:20 +0200

    take 1-4 pages from thesisFile when joining

--- src/util/pdfManipulator.js
+++ src/util/pdfManipulator.js
@@ -3,17 +3,24 @@ const pdftk = require('node-pdftk');
 /**
  * Joins the PDF-files into a single file
  * @param {String} pathToFolder - Absolute path to the file.
- * @param {Array<String>} fileNames - Array of files to be joined.
+ * @param {Array<Object>} attachments - Array of files to be joined.
  * @return {Promise<String>} Promise of the absolute path to the created file.
  */
-export async function joinPdfs(pathToFolder, fileNames) {
+export async function joinPdfs(pathToFolder, attachments) {
     //Does not support past 26.
-    // Bug in node-pdftk or pdftk: trailing and leading spaces are A: ' B ' = 'A B A'
-    const pdfs = fileNames.map((file, index) => String.fromCharCode(65 + index)).join(' ')
+    // Bug in node-pdftk or pdftk: trailing and leading spaces are A: ' B ' === 'A B A'
+    const pdfs = attachments.map((attachment, index) => {
+        //If there are more than one, we only want first 4 pages of thesisFile
+        if (attachments.length > 1 && attachment.label === 'thesisFile') {
+            return String.fromCharCode(65 + index) + '1-4'
+        } else {
+            return String.fromCharCode(65 + index)
+        }
+    }).join(' ')
 
     const input = {};
-    fileNames.forEach((name, index) => {
-        input[String.fromCharCode(65 + index)] = pathToFolder + name;
+    attachments.forEach((attachment, index) => {
+        input[String.fromCharCode(65 + index)] = pathToFolder + attachment.filename;
     })
 
     return pdftk.input(input).cat(pdfs).output()
