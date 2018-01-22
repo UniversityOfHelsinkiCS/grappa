6a4e97b3ff38ff91148282f656d487c16caab0fa
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-01-22 15:42:34 +0200

    Validate invite inputs.

--- src/services/EmailInviteService.js
+++ src/services/EmailInviteService.js
@@ -1,7 +1,19 @@
 const crypto = require('crypto');
+const Checkit = require('checkit');
 const knex = require('../db/connection');
 
 const emailService = require('./EmailService');
+
+const roleInviteType = new Checkit({
+    programme: ['required'],
+    role: ['required'],
+    email: ['required', 'email']
+});
+
+const authorInviteType = new Checkit({
+    email: ['required', 'email'],
+    agreement: ['required']
+});
 
 function generateToken() {
     return crypto.randomBytes(16).toString('hex');
@@ -10,11 +22,17 @@ function generateToken() {
 export async function createEmailInviteForThesisAuthor(email, agreementId, programmeId) {
     const token = generateToken();
     const invite = { email, agreement: agreementId, token, type: 'thesis_author' };
+
+    if (authorInviteType.validateSync(invite)[0]) throw new Error('Invalid parameters');
+
     await knex('emailInvite').insert(invite);
     await emailService.sendInvite(invite, 'thesis', programmeId);
 }
 
-export async function createEmailInviteForRole({ programme, role, email }) {
+export async function createEmailInviteForRole(inviteData) {
+    if (roleInviteType.validateSync(inviteData)[0]) throw new Error('Invalid parameters');
+
+    const { programme, role, email } = inviteData;
     const token = generateToken();
     const invite = { email, role, programme, token, type: 'role' };
     await knex('emailInvite').insert(invite);
