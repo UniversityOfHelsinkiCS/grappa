6227b34a81a79c89ee714b9e5ca03d31a7a044c2
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-01-24 12:19:58 +0200

    Use correct schema for tests.

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -14,14 +14,14 @@ module.exports = {
     test: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
-        searchPath: ['grappa_test', 'public'],
+        searchPath: ['grappa_test'],
         useNullAsDefault: true,
         migrations: {
             directory: './src/db/migrations'
         },
         seeds: {
             directory: './src/db/seeds'
-        },
+        }
     },
     production: {
         client: 'pg',
