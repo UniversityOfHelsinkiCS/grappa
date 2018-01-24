7ecf94ded57330d37dece9b4953d25f7e00f6a28
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-01-24 09:19:40 +0200

    Fixing travis build.

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
     test: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
-        searchPath: ['grappa_test', 'public'],
+        searchPath: ['grappa_test'],
         useNullAsDefault: true,
         migrations: {
             directory: './src/db/migrations'
