e837b43ef7c117c8d2fa3a7cdeb14be65b32fe28
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-01-29 17:43:20 +0200

    Remove unused methods.

--- src/services/ProgrammeService.js
+++ src/services/ProgrammeService.js
@@ -9,14 +9,8 @@ const programmeSchema = [
 export const getAllProgrammes = () =>
     knex.select(programmeSchema).from('programme').then(programme => programme);
 
-export const getProgrammeId = name =>
-    knex.select(programmeSchema).from('programme').where('name', name).first();
-
-export const getProgramme = programmeId =>
-    knex.select(programmeSchema).from('programme').where('programmeId', programmeId).first();
-
 export const getStudyfieldsProgramme = studyfieldId =>
     knex.select(programmeSchema).from('programme')
         .innerJoin('studyfield', 'programme.programmeId', '=', 'studyfield.programmeId')
         .where('studyfield.studyfieldId', studyfieldId)
-        .first()
+        .first();
