c05c99049cb5ba577f9fe1980654429039007639
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-01-31 19:13:53 +0200

    Test thesis list filtering.

--- test/thesis/ThesisList.spec.js
+++ test/thesis/ThesisList.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import test from 'ava';
-import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+
 import ThesisList from '../../src/components/thesis/ThesisList';
 
 const thesis = {
@@ -20,6 +23,24 @@ const thesisApp = (<ThesisList
 />);
 const wrapper = shallow(thesisApp);
 
+const theses = [
+    {
+        authorFirstname: 'Teppo',
+        authorLastname: 'Testaaja',
+        thesisTitle: 'Gradu yksi',
+        grade: '5',
+        thesisId: 1,
+        printDone: false
+    },
+    {
+        authorFirstname: 'Keijo',
+        authorLastname: 'Kokeilija',
+        thesisTitle: 'Gradu kaksi',
+        grade: '5',
+        thesisId: 2,
+        printDone: false
+    }
+];
 
 test('should have a table element', (t) => {
     t.is(wrapper.find('table').length, 1);
@@ -29,3 +50,39 @@ test('should have 2 tr elements', (t) => {
     t.is(wrapper.find('tr').length, 2);
 });
 
+test('theses can be filtered', (t) => {
+    const component = mount(
+        <MemoryRouter>
+            <ThesisList
+                theses={theses}
+                downloadSelected={() => ({})}
+                agreements={[]}
+                attachments={[]}
+            />
+        </MemoryRouter>);
+
+    component.find('.prompt').simulate('change', { target: { value: 'kaksi' } });
+    t.is(component.find('a[href="/thesis/2"]').length, 1);
+    t.is(component.find('a[href="/thesis/1"]').length, 0);
+    component.find('.prompt').simulate('change', { target: { value: '' } });
+    t.is(component.find('a[href="/thesis/1"]').length, 1);
+});
+
+test('theses can be selected to download', (t) => {
+    const download = sinon.spy();
+    const component = mount(
+        <MemoryRouter>
+            <ThesisList
+                theses={theses}
+                downloadSelected={download}
+                agreements={[]}
+                attachments={[]}
+            />
+        </MemoryRouter>);
+
+    component.find('input[type="checkbox"]').at(1).simulate('change', { target: { checked: true } });
+    component.find('input[type="checkbox"]').at(2).simulate('change', { target: { checked: true } });
+    component.find('.orange').simulate('click');
+
+    t.true(download.called);
+});
