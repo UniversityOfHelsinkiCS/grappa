045280e943af5537abd28b6e1f975a5e0c3eb280
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-02-02 14:22:15 +0200

    Add cat output for tests.

--- src/util/logger.js
+++ src/util/logger.js
@@ -4,23 +4,25 @@ const { combine, timestamp } = format;
 const fileFormat = combine(timestamp(), format.json());
 
 const logger = createLogger({
-    level: 'debug',
-    transports: [new transports.Console({ format: format.simple() })]
+    transports: [
+        new transports.File({
+            level: 'info',
+            format: fileFormat,
+            filename: './logs/grappa.log'
+        })
+    ]
 });
 
+if (process.env.KISSA !== 'true') {
+    logger.add(new transports.Console({ level: 'debug', format: format.simple() }));
+}
 
 if (process.env.NODE_ENV !== 'test') {
-    logger.add(new transports.File({
-        level: 'info',
-        format: fileFormat,
-        filename: './logs/grappa.log'
-    }));
     logger.add(new transports.File({
         level: 'error',
         format: fileFormat,
         filename: './logs/grappa.error.log'
     }));
-
 }
 
 module.exports = logger;
