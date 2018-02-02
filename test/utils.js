e0c14224d78b1c76e86976907bef04a50d274fa3
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-02-02 10:11:48 +0200

    Don't drop schema for beginning of test.

--- test/utils.js
+++ test/utils.js
@@ -15,45 +15,42 @@ export async function createPerson(email) {
 }
 
 export async function deleteFromDb(connection) {
-    return Promise.all([
-        connection('meetingProgramme').del(),
-        connection('previousagreements').del(),
-        connection('agreementDraftPerson').del(),
-        connection('agreementDraft').del(),
-        connection('notification').del(),
-        connection('emailDraft').del(),
-        connection('attachment').del(),
-        connection('agreementPerson').del(),
-        connection('emailInvite').del(),
-        connection('agreement').del(),
-        connection('thesis').del(),
-        connection('personWithRole').del(),
-        connection('person').del(),
-        connection('role').del(),
-        connection('councilmeeting').del(),
-        connection('studyfield').del(),
-        connection('programme').del(),
-        connection('faculty').del(),
+    await connection('meetingProgramme').del(),
+    await connection('previousagreements').del(),
+    await connection('agreementDraftPerson').del(),
+    await connection('agreementDraft').del(),
+    await connection('notification').del(),
+    await connection('emailDraft').del(),
+    await connection('attachment').del(),
+    await connection('agreementPerson').del(),
+    await connection('emailInvite').del(),
+    await connection('agreement').del(),
+    await connection('thesis').del(),
+    await connection('personWithRole').del(),
+    await connection('person').del(),
+    await connection('role').del(),
+    await connection('councilmeeting').del(),
+    await connection('studyfield').del(),
+    await connection('programme').del(),
+    await connection('faculty').del(),
 
-        connection.raw('alter sequence "agreementDraft_agreementDraftId_seq" restart with 4'),
-        connection.raw('alter sequence "agreement_agreementId_seq" restart with 4'),
-        connection.raw('alter sequence "attachment_attachmentId_seq" restart with 2'),
-        connection.raw('alter sequence "councilmeeting_councilmeetingId_seq" restart with 2'),
-        connection.raw('alter sequence "notification_notificationId_seq" restart with 2'),
-        connection.raw('alter sequence "personWithRole_personRoleId_seq" restart with 17'),
-        connection.raw('alter sequence "person_personId_seq" restart with 20'),
-        connection.raw('alter sequence "programme_programmeId_seq" restart with 9'),
-        connection.raw('alter sequence "studyfield_studyfieldId_seq" restart with 8'),
-        connection.raw('alter sequence "thesis_thesisId_seq" restart with 5')
-    ]);
+    await connection.raw('alter sequence "agreementDraft_agreementDraftId_seq" restart with 4'),
+    await connection.raw('alter sequence "agreement_agreementId_seq" restart with 4'),
+    await connection.raw('alter sequence "attachment_attachmentId_seq" restart with 2'),
+    await connection.raw('alter sequence "councilmeeting_councilmeetingId_seq" restart with 2'),
+    await connection.raw('alter sequence "notification_notificationId_seq" restart with 2'),
+    await connection.raw('alter sequence "personWithRole_personRoleId_seq" restart with 17'),
+    await connection.raw('alter sequence "person_personId_seq" restart with 20'),
+    await connection.raw('alter sequence "programme_programmeId_seq" restart with 9'),
+    await connection.raw('alter sequence "studyfield_studyfieldId_seq" restart with 8'),
+    await connection.raw('alter sequence "thesis_thesisId_seq" restart with 5')
 }
 
 export async function initDb() {
     const schema = process.env.DB_SCHEMA;
     const connection = knex.getKnex();
 
-    await connection.raw(`drop schema if exists ${schema} cascade`);
-    await connection.raw(`create schema ${schema}`);
+    await connection.raw(`create schema if not exists ${schema}`);
     await connection.migrate.latest();
     await deleteFromDb(connection);
     await connection.seed.run();
