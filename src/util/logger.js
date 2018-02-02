84d7d40ea6d0f808210a0e87a8ef1396a7f6df25
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-02-02 14:37:25 +0200

    Keep tap output for travis.

--- src/util/logger.js
+++ src/util/logger.js
@@ -13,11 +13,8 @@ const logger = createLogger({
     ]
 });
 
-if (process.env.KISSA !== 'true') {
-    logger.add(new transports.Console({ level: 'debug', format: format.simple() }));
-}
-
 if (process.env.NODE_ENV !== 'test') {
+    logger.add(new transports.Console({ level: 'debug', format: format.simple() }));
     logger.add(new transports.File({
         level: 'error',
         format: fileFormat,
