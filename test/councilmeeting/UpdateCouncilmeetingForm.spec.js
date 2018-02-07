85ec667a5365bec2a9a0816483c44a96a2dee0ff
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-02-07 11:17:20 +0200

    More tests for councilmeeting.

--- test/councilmeeting/UpdateCouncilmeetingForm.spec.js
+++ test/councilmeeting/UpdateCouncilmeetingForm.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import test from 'ava';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 import moment from 'moment';
 import UpdateCouncilmeetingForm from '../../src/components/councilmeeting/UpdateCouncilmeetingForm';
@@ -16,17 +16,17 @@ test('Councilmeeting can be updated', (t) => {
         councilmeetingId: 1,
         programmes: []
     };
-    const newCouncilmeeting = shallow(
+    const compopnent = shallow(
         <UpdateCouncilmeetingForm
             meeting={meeting}
             updateMeeting={updateMeeting}
             programmes={programmes}
         />
     );
-    newCouncilmeeting.find('DatePicker').at(0).simulate('change', moment('2018-01-12'));
-    newCouncilmeeting.find('DatePicker').at(1).simulate('change', moment('2018-01-11'));
-    newCouncilmeeting.find('DatePicker').at(2).simulate('change', moment('2018-01-10'));
-    newCouncilmeeting.find('button').simulate('click');
+    compopnent.find('DatePicker').at(0).simulate('change', moment('2018-01-12'));
+    compopnent.find('DatePicker').at(1).simulate('change', moment('2018-01-11'));
+    compopnent.find('DatePicker').at(2).simulate('change', moment('2018-01-10'));
+    compopnent.find('button').simulate('click');
 
     t.true(updateMeeting.called);
 
@@ -36,6 +36,50 @@ test('Councilmeeting can be updated', (t) => {
     t.is(data.date.format(format), '2018-01-12');
     t.is(data.instructorDeadline.format(format), '2018-01-11');
     t.is(data.studentDeadline.format(format), '2018-01-10');
-    // t.is(data.programmes[0], 1);
     t.is(data.councilmeetingId, 1);
+});
+
+test('selected programmes are listed', (t) => {
+    const updateMeeting = sinon.spy();
+    const meeting = {
+        date: '2017-11-29T22:00:00.000Z',
+        instructorDeadline: '2017-11-20T22:00:00.000Z',
+        studentDeadline: '2017-11-10T22:00:00.000Z',
+        councilmeetingId: 1,
+        programmes: [programmes[0]]
+    };
+    const component = mount(
+        <UpdateCouncilmeetingForm
+            meeting={meeting}
+            updateMeeting={updateMeeting}
+            programmes={programmes}
+        />
+    );
+
+    t.is(component.find('td').length, 2);
+});
+
+test('programmes can be changed', (t) => {
+    const updateMeeting = sinon.spy();
+    const meeting = {
+        date: '2017-11-29T22:00:00.000Z',
+        instructorDeadline: '2017-11-20T22:00:00.000Z',
+        studentDeadline: '2017-11-10T22:00:00.000Z',
+        councilmeetingId: 1,
+        programmes: [programmes[0]]
+    };
+    const component = mount(
+        <UpdateCouncilmeetingForm
+            meeting={meeting}
+            updateMeeting={updateMeeting}
+            programmes={programmes}
+        />
+    );
+
+    t.is(component.find('td').length, 2);
+    component.find('.remove').simulate('click');
+    t.is(component.find('td').length, 0);
+
+    component.find('select').simulate('change', { target: { value: '1' } });
+    t.is(component.find('td').length, 2);
 });
