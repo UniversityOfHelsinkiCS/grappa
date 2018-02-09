9a73ed4ec566f59d40b093eaf9abbf998423e4a6
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-02-09 16:51:14 +0200

    Revert changes.

--- src/db/migrations/2018013001_alter_table.js
+++ src/db/migrations/2018013001_alter_table.js
@@ -1,11 +1,9 @@
 exports.up = knex => Promise.all([
-    knex.schema.hasTable('agreement').then(() => {
-        knex.schema.alterTable('agreement', (table) => {
-            table
-                .date('startDate')
-                .defaultTo(knex.fn.now())
-                .alter();
-        })
+    knex.schema.alterTable('agreement', (table) => {
+        table
+            .date('startDate')
+            .defaultTo(knex.fn.now())
+            .alter();
     })
 ]);
 
