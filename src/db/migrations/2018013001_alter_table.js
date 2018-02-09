67f5ab57ad7355466578df1e6fa0b5f94c3fd3ad
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-02-09 16:41:49 +0200

    Trying to fix knex warning with migration file.

--- src/db/migrations/2018013001_alter_table.js
+++ src/db/migrations/2018013001_alter_table.js
@@ -1,9 +1,11 @@
 exports.up = knex => Promise.all([
-    knex.schema.alterTable('agreement', (table) => {
-        table
-            .date('startDate')
-            .defaultTo(knex.fn.now())
-            .alter();
+    knex.schema.hasTable('agreement').then(() => {
+        knex.schema.alterTable('agreement', (table) => {
+            table
+                .date('startDate')
+                .defaultTo(knex.fn.now())
+                .alter();
+        })
     })
 ]);
 
