efac775841f2f07f6410cf97fc72d2c73af482c4
Jami Kousa <jakousa@cs.helsinki.fi>
2018-02-19 18:12:04 +0200

    info in invite-error

--- src/containers/Invite/index.js
+++ src/containers/Invite/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { string, shape, bool, func } from 'prop-types'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { Redirect } from 'react-router'
 
 import { acceptThesis, acceptRole } from './services/inviteAction'
@@ -17,11 +18,32 @@ const InvitePage = ({ acceptThesisAction, acceptRoleAction, match, status }) => 
     }
 
     if (status) {
-        return <Redirect to="/" />
+        return <Redirect to="/theses" />
     }
-
+    // Error:
     return (
-        <div />
+        <div>
+            <h3>Welcome, do not panic</h3>
+            <p>
+                You should not see this page if there was not an error message above.
+                You came here through a link and the link was already used, you probably clicked it once before.
+            </p>
+            <p>
+                If you are a student, check the <Link to="/theses">Thesis List</Link> page.
+                You should see one thesis and by clicking that thesis you should see correct data.
+            </p>
+            <p>
+                If the data is not correct, contact your supervisor.
+            </p>
+            <p>
+                If you are not a student:
+                you should see your information on the <Link to="/">Homepage</Link> which has been updated accordingly
+            </p>
+            <p>
+                If Grappa (this application) behaves in a strange way contact grp-toska@helsinki.fi
+            </p>
+
+        </div>
     )
 }
 
