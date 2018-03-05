75048bf8dcd999668bbdf49b86c1e94cf3215269
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-03-05 14:46:26 +0200

    Fix invite urls.

--- src/containers/Invite/services/inviteAction.js
+++ src/containers/Invite/services/inviteAction.js
@@ -1,6 +1,4 @@
 import { callController } from '../../../util/apiConnection'
 
-const prefix = process.env.NODE_ENV === 'development' ? '' : '../..'
-
-export const acceptThesis = token => callController(`${prefix}/invite/thesis/${token}`, 'INVITE_ACCEPT_THESIS_')
-export const acceptRole = token => callController(`${prefix}/invite/role/${token}`, 'INVITE_ACCEPT_ROLE_')
+export const acceptThesis = token => callController(`/invite/thesis/${token}`, 'INVITE_ACCEPT_THESIS_')
+export const acceptRole = token => callController(`/invite/role/${token}`, 'INVITE_ACCEPT_ROLE_')
