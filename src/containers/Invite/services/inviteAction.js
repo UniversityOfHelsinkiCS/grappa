22342e24bbdf38a287dc91bf5dcd1778bc364a01
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-03-05 14:05:09 +0200

    Fix thesis invite on staging.

--- src/containers/Invite/services/inviteAction.js
+++ src/containers/Invite/services/inviteAction.js
@@ -1,4 +1,6 @@
 import { callController } from '../../../util/apiConnection'
 
-export const acceptThesis = token => callController(`/invite/thesis/${token}`, 'INVITE_ACCEPT_THESIS_')
-export const acceptRole = token => callController(`/invite/role/${token}`, 'INVITE_ACCEPT_ROLE_')
+const prefix = process.env.NODE_ENV === 'development' ? '' : '../..'
+
+export const acceptThesis = token => callController(`${prefix}/invite/thesis/${token}`, 'INVITE_ACCEPT_THESIS_')
+export const acceptRole = token => callController(`${prefix}/invite/role/${token}`, 'INVITE_ACCEPT_ROLE_')
