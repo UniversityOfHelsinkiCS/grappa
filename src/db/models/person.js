ae0d002d733d45245bc189fab4d8697a81463ea2
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-04-25 14:57:40 +0300

    changed person model

--- src/db/models/person.js
+++ src/db/models/person.js
@@ -1,7 +1,11 @@
 const bookshelf = require('../bookshelf')
+const PersonWithRole = require('./person_with_role')
 
 const Person = bookshelf.Model.extend({
     tableName: 'person',
-    idAttribute: 'personId'
+    idAttribute: 'personId',
+    roles() {
+        return this.hasMany(PersonWithRole, 'personId', 'personId')
+    }
 })
 module.exports = Person
