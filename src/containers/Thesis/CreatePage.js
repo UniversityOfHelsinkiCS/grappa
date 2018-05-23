6a4e56579d1f1474d0384f84a21e22e88fb73062
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-05-23 16:15:58 +0300

    changed council meeting selection to depend on selected thesis programme

--- src/containers/Thesis/CreatePage.js
+++ src/containers/Thesis/CreatePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { arrayOf, func } from 'prop-types'
 import { connect } from 'react-redux'
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown, Message } from 'semantic-ui-react'
+import moment from 'moment'
 
 import { saveThesis } from './services/thesisActions'
 import { requestGraderAction, getGradersAction } from '../Person/services/personActions'
@@ -10,7 +11,7 @@ import { personType, programmeType, studyfieldType, councilmeetingType } from '.
 import ThesisConfirmModal from './components/ThesisConfirmModal'
 import ThesisInformation from './components/ThesisInformation'
 import AttachmentAdder from '../Attachment/components/AttachmentAdder'
-import ThesisCouncilMeetingPicker from './components/ThesisCouncilmeetingPicker'
+// import ThesisCouncilMeetingPicker from './components/ThesisCouncilmeetingPicker'
 import { emptyThesisData, thesisValidation } from '../../util/theses'
 import LoadingIndicator from '../LoadingIndicator'
 import AddOutsidePerson from '../Person/components/AddOutsidePerson'
@@ -37,10 +38,10 @@ export class ThesisCreatePage extends Component {
         thesis.graders = thesis.graders.map(person => person.personId)
         form.append('json', JSON.stringify(thesis))
         this.setState({ showModal: !this.state.showModal }, this.props.saveThesis(form))
-    };
+    }
 
     validateAttachments = attachments => attachments.find(attachment => attachment.label === 'thesisFile')
-        && attachments.find(attachment => attachment.label === 'reviewFile');
+        && attachments.find(attachment => attachment.label === 'reviewFile')
 
     toggleModal = () => {
         this.validateThesis()
@@ -52,7 +53,7 @@ export class ThesisCreatePage extends Component {
                 }
             })
             .catch(res => this.setState({ validationErrors: res.errors }))
-    };
+    }
 
     handleChange = (changedValues) => {
         const thesis = Object.assign({}, this.state.thesis, changedValues)
@@ -64,11 +65,11 @@ export class ThesisCreatePage extends Component {
         this.validateThesis(thesis)
             .then(() => this.setState({ validationErrors: {} }))
             .catch(res => this.setState({ validationErrors: res.errors }))
-    };
+    }
 
     editAttachmentList = (attachments) => {
         this.setState({ attachments })
-    };
+    }
 
     validateThesis(thesis = this.state.thesis) {
         return thesisValidation.run(thesis)
@@ -86,6 +87,32 @@ export class ThesisCreatePage extends Component {
     changeGraders = (e, data) => {
         const graders = this.props.graders.filter(grader => data.value.includes(grader.personId))
         this.handleChange({ graders })
+    }
+
+
+    formatMeetings = () => {
+        const { councilmeetings } = this.props
+        const programmeId = parseInt(this.state.thesis.programmeId, 10)
+
+        if (!councilmeetings || !programmeId)
+            return []
+
+        // Deadline is always at the end of the day, so if day is either same or after, then it's not past the deadline.
+        const isInFuture = meeting => moment(meeting.instructorDeadline).isSameOrAfter(moment(), 'day')
+        const formatDate = meeting => moment(meeting.date).format('DD.MM.YYYY')
+        const formatDeadline = meeting => moment(meeting.instructorDeadline).format('23:59 DD.MM.YYYY')
+        const isMeetingSelectable = meeting => isInFuture(meeting) && meeting.programmes.includes(programmeId)
+
+        const meetings = councilmeetings
+            .filter(isMeetingSelectable)
+            .map(meeting => ({
+                name: 'councilmeetingId',
+                value: meeting.councilmeetingId,
+                key: meeting.councilmeetingId,
+                id: meeting.councilmeetingId,
+                text: `${formatDate(meeting)} Deadline: ${formatDeadline(meeting)}`
+            }))
+        return meetings
     }
 
     renderGraderSelector = () => {
@@ -139,12 +166,25 @@ export class ThesisCreatePage extends Component {
                         changeList={this.editAttachmentList}
                     />
                     <br />
+                    <div className="two fields">
+                        <div className="field">
+                            {programme ?
+                                <Message color="teal">{programme.name}</Message> :
+                                <Message color="red">Please select the unit first</Message>
+                            }
+                        </div>
+                        <div className="field">
+                            <Dropdown placeholder="Select meeting" fluid selection options={this.formatMeetings()} onChange={(e, data) => this.handleChange({ councilmeetingId: data.value })} />
+                        </div>
+                    </div>
+                    {/*
                     <ThesisCouncilMeetingPicker
                         sendChange={this.handleChange}
                         chosenMeetingId={this.state.thesis.councilmeetingId}
                         councilmeetings={this.props.councilmeetings}
                         programmes={this.props.programmes}
                     />
+                    */}
                 </div>
                 <br />
                 <button className="ui positive button" onClick={this.toggleModal}>
