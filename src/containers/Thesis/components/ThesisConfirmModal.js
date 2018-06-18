f199db63b2ede97e69ce822acf7ba323067b21c7
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-06-18 13:22:17 +0300

    Change thesis confirm modal

--- src/containers/Thesis/components/ThesisConfirmModal.js
+++ src/containers/Thesis/components/ThesisConfirmModal.js
@@ -1,56 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { bool, func } from 'prop-types'
+import { Button } from 'semantic-ui-react'
 
-class ThesisConfirmModal extends Component {
-    state = {
-        buttonDisabled: ''
+const ThesisConfirmModal = ({ showModal, closeModal, sendSaveThesis }) => {
+    if (!showModal) {
+        return null
     }
-
-    handleSaveThesis = () =>
-        this.setState({ buttonDisabled: 'disabled' }, this.props.sendSaveThesis())
-
-
-    render() {
-        const { showModal, closeModal } = this.props
-
-        if (!showModal) {
-            return null
-        }
-        return (
-            <div>
-                <div className="ui dimmer modals page transition visible active" onClick={closeModal} />
-                <div className="ui active modal" style={{ border: '2px solid black', borderRadius: '7px' }}>
-                    <i className="close icon" onClick={closeModal} />
-                    <div className="header">
-                        Reminder
-                    </div>
-                    <div style={{ margin: '1%' }}>
-                        <div className="description">
-                            <p>
-                                If you are from department of CS:
-                                Have you remembered to add the thesis into the thesis-management system?
-                                If not please do so right away.
-                            </p>
-                            <a
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                href="https://ilmo.cs.helsinki.fi/gradu/servlet/hae"
-                            >
-                                Ilmo (opens in a new window)
-                            </a>
-                        </div>
-                        <br />
-                        <button
-                            className={`ui fluid positive ${this.state.buttonDisabled} button`}
-                            onClick={this.handleSaveThesis}
-                        >
-                            Confirm save
-                        </button>
-                    </div>
+    return (
+        <div>
+            <div className="ui dimmer modals page transition visible active" onClick={closeModal} />
+            <div className="ui active modal" style={{ border: '2px solid black', borderRadius: '7px' }}>
+                <i className="close icon" onClick={closeModal} />
+                <div className="header">
+                    Reminder
                 </div>
-            </div >
-        )
-    }
+                <div style={{ margin: '1%' }}>
+                    <div className="description">
+                        <p>
+                            If you are from department of CS:
+                            Have you remembered to add the thesis into the thesis-management system?
+                            If not please do so right away.
+                        </p>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://ilmo.cs.helsinki.fi/gradu/servlet/hae"
+                        >
+                            Ilmo (opens in a new window)
+                        </a>
+                    </div>
+                    <br />
+                    <Button negative onClick={closeModal}>Cancel</Button>
+                    <Button positive onClick={sendSaveThesis}>Confirm save</Button>
+                </div>
+            </div>
+        </div >
+    )
 }
 
 ThesisConfirmModal.propTypes = {
