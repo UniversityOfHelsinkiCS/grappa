df8e09cbf86f2e2ebd49811df0fc082683752899
Jami Kousa <jami.kousa@helsinki.fi>
2020-04-29 17:24:49 +0300

    bump winston version

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,30 +1,29 @@
-import winston from 'winston'
-import 'winston-log2gelf'
+const winston = require('winston')
+const Log2gelf = require('winston-log2gelf')
 
-const logTransports = []
-
-// Log to console when running unit tests if CONSOLE_OUTPUT=true
-if (process.env.CONSOLE_OUTPUT === 'true') {
-    logTransports.push(new winston.transports.Console({ level: 'debug' }))
-}
+const transports = []
 
 if (process.env.NODE_ENV !== 'test') {
-    logTransports.push(new winston.transports.Console({ level: 'debug' }))
+    transports.push(new winston.transports.File({ filename: 'debug.log' }))
 }
 
 if (process.env.LOG_PORT && process.env.LOG_HOST) {
-    logTransports.push(new winston.transports.Log2gelf({
-        hostname: process.env.LOG_HOSTNAME || 'grappa2-backend',
-        host: process.env.LOG_HOST,
-        port: process.env.LOG_PORT,
-        protocol: process.env.LOG_PROTOCOL || 'https',
-        environment: process.env.NODE_ENV,
-        protocolOptions: {
-            path: process.env.LOG_PATH || '/gelf'
-        }
-    }))
+    transports.push(
+        new Log2gelf({
+            hostname: process.env.LOG_HOSTNAME || 'grappa2-backend',
+            host: process.env.LOG_HOST,
+            port: process.env.LOG_PORT,
+            protocol: process.env.LOG_PROTOCOL || 'https',
+            environment: process.env.NODE_ENV,
+            protocolOptions: {
+                path: process.env.LOG_PATH || '/gelf'
+            }
+        })
+    )
 }
 
-const logger = new winston.Logger({ transports: logTransports })
+transports.push(new winston.transports.Console({ level: 'debug' }))
+
+const logger = winston.createLogger({ transports })
 
 module.exports = logger
