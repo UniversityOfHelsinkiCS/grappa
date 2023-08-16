c7ce204bb66bf0c094ceeec7234851e4c6dffe0e
ShootingStar91 <ttu91@hotmail.com>
2023-08-16 11:53:13 +0300

    Fix crashing

--- backend/src/middleware/auditLogger.js
+++ backend/src/middleware/auditLogger.js
@@ -5,7 +5,7 @@ const loggedMethods = ['POST', 'PUT', 'DELETE']
 
 module.exports = function auditLogger(req, res, next) {
     console.log("Req: " + req.method + " " + req.originalUrl + " at " + new Date().toString())
-    if (req?.body) console.log("body keys: " + Object.keys(req.body))
+    if (req.body) console.log("body keys: " + Object.keys(req.body))
     if (loggedMethods.includes(req.method)) {
         const type = `${req.method} ${req.originalUrl}`
 
