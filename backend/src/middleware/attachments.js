10c3741ac6e5041561439bf63af7f93d659158fe
ShootingStar91 <ttu91@hotmail.com>
2023-08-16 10:52:04 +0300

    add logging to upload

--- backend/src/middleware/attachments.js
+++ backend/src/middleware/attachments.js
@@ -3,6 +3,7 @@ import multer from 'multer'
 const PATH_TO_FOLDER = '/usr/src/app/uploads/'
 
 const storage = () => {
+    console.log("Storage starting")
     try {
         if (process.env.NODE_ENV === 'test') {
             return multer.memoryStorage()
@@ -11,16 +12,25 @@ const storage = () => {
             destination: PATH_TO_FOLDER
         })
     } catch (e) {
-        console.log("storage method failed. Error starting from next line")
+        console.log("storage failed. Error starting from next line")
         console.log(e)
         throw e
     }
 }
 
-const upload = multer({ storage: storage() }).fields([
+const upload = () => {
+    console.log("Starting upload")
+    try {
+    multer({ storage: storage() }).fields([
     { name: 'otherFile' },
     { name: 'reviewFile', maxCount: 1 },
     { name: 'thesisFile', maxCount: 1 }
 ])
+    } catch (e) {
+        console.log("Upload failed. Error: ")
+        console.log(e)
+        throw e
+    }
+}
 
 module.exports = upload
