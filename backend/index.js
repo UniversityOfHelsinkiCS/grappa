dd67d2f0ade189e76066a9dc5dea50ee4c03132f
ShootingStar91 <ttu91@hotmail.com>
2023-08-16 10:32:34 +0300

    Add version log

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const server = require('http').createServer(app)
 
 app.listen(3100, () => {
     logger.info('Grappa app listening on port 3100!')
-    logger.info('Debugging version: 0.0.1')
+    logger.info('Debugging version: 0.0.3')
     logger.info(`Environment is ${process.env.NODE_ENV}`)
 })
 
